d5cbc901fee7a41eb2421aa07d35807fa5681f2b
kachilala <kachilala23@gmail.com>
2020-12-02 15:23:01 +0100

    made use of context in a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
 import Counter from "./Counter";
 import CounterHooks from "./CounterHooks";
 
+export const ThemeContext = React.createContext();
+
+
 function App() {
+  const [theme, setTheme] = useState('red');
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+    Counter
     <Counter initialCount={7} />
+    Counter Hooks
     <CounterHooks initialCount={0} />
-    </>
+    <button onClick={() => setTheme(prevTheme => {
+      return prevTheme === 'red' ? 'blue' : 'red'
+    })}>Toggle Button</button>
+    </ThemeContext.Provider>
   )
 }
 
