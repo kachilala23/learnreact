5aadc178d36ed5761cb17bb2653baa23c1c81f80
kachilala <kachilala23@gmail.com>
2020-12-02 14:45:47 +0100

    add functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
 
 function App() {
   return (
+    <>
     <Counter initialCount={7} />
+    <CounterHooks initialCount={0} />
+    </>
   )
 }
 
